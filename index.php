d174a59f8a0905a79c831e64381d97577d0e2d37
vihoangson <vihoangson@gmail.com>
2019-04-08 06:12:16 +0700

    commit code

--- index.php
+++ index.php
@@ -3,8 +3,16 @@ use Sunra\PhpSimple\HtmlDomParser;
 use Symfony\Component\DomCrawler\Crawler;
 
 include_once ('vendor/autoload.php');
-$file_name = 'https://ticketbox.vn/EventList/EventList/LoadEventList?Q=&CityName=&OtherLocation=false&CityId=&Categories=&From=&To=&DateFilter=&Price=3&SiteId=1&CultureName=&Offset=0&Limit=3&TotalCount=3&categories=';
-$str = file_get_contents($file_name);
+//unlink('mm');
+if(!file_exists('mm')){
+    $limit  = 20;
+    $file_name = 'https://ticketbox.vn/EventList/EventList/LoadEventList?Q=&CityName=&OtherLocation=false&CityId=&Categories=&From=&To=&DateFilter=&Price=3&SiteId=1&CultureName=&Offset=0&Limit='.$limit.'&TotalCount=3&categories=';
+    $str = file_get_contents($file_name);
+    file_put_contents('mm',$str);
+}else{
+    $str = file_get_contents('mm');
+}
+
 if(false){
     $dom = HtmlDomParser::str_get_html( $str );
     //$dom = HtmlDomParser::file_get_html( $file_name );
@@ -13,8 +21,11 @@ if(false){
 }
 
 $crawler = new Crawler($str);
-var_dump($crawler);
+$crawler->filter('a')->first();
+//var_dump($crawler);
+var_dump($crawler->filter('a')->nextAll()->html());
 
-foreach ($crawler as $domElement) {
-//    var_dump($domElement->text());
-}
+//
+// foreach ($crawler->filter('a') as $domElement) {
+//     var_dump($domElement->html());
+// }
